0b0ce7c72c7210dda7a62ca20261d8ad821a5b41
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-12 11:38:27 +0400

    !

--- Promises_async_fetch/books.js
+++ Promises_async_fetch/books.js
@@ -4,9 +4,24 @@ const source = "https://openlibrary.org/search.json?q=python";
 
 async function getBooks() {
     
-    const response = fetch(source);
+    try {
 
-    const books = await response.json();
+        const response = fetch(source);
 
+        const books = await response.json().slice(0,10);
+
+        books.forEach(book => {
+            console.log(`Title: ${book.title}\n`);
+            console.log(`Author: ${book.author}\n`);
+            console.log(`Year: ${book.year}/n`)
+        });
+
+    } catch(error) {
+
+        console.log("Error: ", error)
+    }
     
+
+
+
 }
