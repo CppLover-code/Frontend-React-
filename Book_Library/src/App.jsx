e305d128913f27fb3a00f61bc9a93ee77254e538
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-15 11:34:57 +0400

    !

--- Book_Library/src/App.jsx
+++ Book_Library/src/App.jsx
@@ -1,5 +1,26 @@
+import Book from "./components/Book";
+
+// Компонент — это обычная JavaScript-функция, которая возвращает JSX.
+// App - это компонент.
 function App() {
   return (
-      <h1>Hello React</h1>
+    <div>
+      <h1>My Book Library</h1>
+
+      <Book
+        title="Python"
+        author="Will Smith"
+      />
+      <Book
+        title="C++"
+        author="Jennifer Lopez"
+      />
+      <Book
+        title="Java Script"
+        author="Michael Jackson"
+      />
+    </div>
   );
 }
+ export default App;
+
---
+++ Book_Library/src/components/book.jsx
@@ -0,0 +1,23 @@
+// props - параметры
+/*function Book(props) {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <p>Автор: {props.author}</p>
+        </div>
+    );
+}
+
+export default Book;
+*/
+
+function Book({title, author}) {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <p>Автор: {author}</p>
+        </div>
+    );
+}
+
+export default Book;
