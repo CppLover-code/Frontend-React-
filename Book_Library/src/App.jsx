fcbb54b7778b929ed4c0d5cee8e1fce7a2a34766
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-16 10:59:48 +0400

    !

--- Book_Library/src/App.jsx
+++ Book_Library/src/App.jsx
@@ -9,22 +9,40 @@ function App() {
   // И setCount - Функцию изменения значения. Начальное значение: 0
   const [count, setCount] = useState(0);
 
+  const [books, setBooks] = useState ([
+    {
+      id: 1,
+      title: "Python",
+      author: "Will Smith"
+    },
+    {
+      id: 2,
+      title: "Java Script",
+      author: "Michael Jackson"
+    },
+    {
+      id: 3,
+      title: "C++",
+      author: "Jennifer Lopez"
+    }
+
+  ]);
+
   return (
     <div>
       <h1>My Book Library</h1>
 
-      <Book
-        title="Python"
-        author="Will Smith"
-      />
-      <Book
-        title="C++"
-        author="Jennifer Lopez"
-      />
-      <Book
-        title="Java Script"
-        author="Michael Jackson"
-      />
+      {books.map(book => (
+        <Book
+        key={book.id}
+        title={book.title}
+        author={book.author}
+        />
+      ))}
+
+      <button onClick={addBook}>
+        Add book
+      </button>
 
       <h1>Counter</h1>
 
@@ -36,6 +54,17 @@ function App() {
     </div>
 
   );
+
+  function addBook() {
+
+  const newBook = {
+    id:books.length + 1,
+    title: "New Book",
+    author: "Unknown"
+  };
+
+  setBooks([...books, newBook]) // оператор spread
+
+  }
 }
  export default App;
-
