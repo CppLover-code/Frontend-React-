57020e800464620ef79f7ea8cf22eeb9490ee943
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-06-16 09:43:14 +0400

    hooks

--- Book_Library/src/App.jsx
+++ Book_Library/src/App.jsx
@@ -1,8 +1,14 @@
 import Book from "./components/Book";
+import { useState } from "react";
 
 // Компонент — это обычная JavaScript-функция, которая возвращает JSX.
 // App - это компонент.
 function App() {
+
+  // React создаёт count - Текущее значение.
+  // И setCount - Функцию изменения значения. Начальное значение: 0
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>My Book Library</h1>
@@ -19,7 +25,16 @@ function App() {
         title="Java Script"
         author="Michael Jackson"
       />
+
+      <h1>Counter</h1>
+
+      <p>{count}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        +
+      </button>
     </div>
+
   );
 }
  export default App;
