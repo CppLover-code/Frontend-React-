45f22ad85637b9c4a66feb82a740be693c7279c7
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-08 11:15:18 +0400

    cart context

--- Book_Library/src/contexts/CartContext.jsx
+++ Book_Library/src/contexts/CartContext.jsx
@@ -1,12 +1,85 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const CartContext = createContext();
 
 // children здесь — это не данные корзины, 
 // а те компоненты, которым мы хотим дать доступ к корзине
-function CartProvider({children})
-{
+// <CartContext.Provider> Это специальный компонент, который React автоматически создаёт после вызова createContext();
+// Через prop value Provider передает данные всем компонентам внутри {children}.
+function CartProvider({ children }) {
+    const [cart, setCart] = useState([]);
 
+    function addToCart(book) {
+
+        const existingBook = cart.find(item => item.id === book.id);
+
+        if (!existingBook) {
+            setCart([...cart, { ...book, quantity: 1 }]);
+
+            return;
+        }
+
+        setCart(
+            cart.map(item => {
+                if (item.id === book.id) {
+                    return {
+                        ...item,
+                        quantity: item.quantity + 1
+                    }
+                }
+
+                return item;
+            })
+        );
+    }
+
+    function removeFromCart(id) {
+        setCart(cart.filter(item => item.id !== id));
+    }
+
+    function decreaseQuantity(id) {
+        const decItem = cart.find(item => item.id === id)
+
+        if (!decItem) return;
+
+        if (decItem.quantity === 1) {
+            removeFromCart(decItem.id);
+            return;
+        }
+
+        setCart(
+            cart.map(item => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        quantity: item.quantity - 1
+                    }
+                }
+
+                return item;
+            })
+        );
+    }
+
+    function increaseQuantity(id) {
+        setCart(
+            cart.map(item => {
+                if (item.id === id) {
+                    return {
+                        ...item,
+                        quantity: item.quantity + 1
+                    }
+                }
+
+                return item;
+            })
+        );
+    }
+    return (
+        <CartContext.Provider value={null}>
+            {children}
+        </CartContext.Provider>
+    );
 }
 
 export { CartContext, CartProvider };
