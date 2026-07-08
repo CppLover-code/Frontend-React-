a2ead2ac132976687e4132012eb9ff880e4cb60a
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-08 14:06:25 +0400

    book context

--- Book_Library/src/App.jsx
+++ Book_Library/src/App.jsx
@@ -16,72 +16,7 @@ import MainLayout from "./layouts/MainLayout";
 // App - это компонент.
 function App() 
 {
-  // при первом взапуске приложения реакт проверяет состояния - состояния нет,
-  // поэтому вызывает функцию и берет ее результат как начальное значение
-  // при втором запуске - состояние уже есть, поэтому реакт будет игнорировать эту функцию
-  // и просто возвращает сохраненное состояние
-  const [books, setBooks] = useState(() => {
-
-    const savedBooks = localStorage.getItem("books");
-
-    if(savedBooks) return JSON.parse(savedBooks);
-
-    return initialBooks;
-
-  });
-
-  // каждый раз, когда меняется books, книги сохраняются
-  useEffect(() => {
-
-    localStorage.setItem(
-      "books",
-      JSON.stringify(books)
-    );
-
-  }, [books]);
-
-  function addBook(title, authors, category, price) 
-  {
-    if(!title.trim()) return;
-    
-    const newBook = 
-    {
-      id:books.length + 1,
-      title: title,
-      authors: authors || "Unknown",
-      category: category || "Unknown",
-      price: price || 0.00
-    };
-
-    setBooks([...books, newBook]) // оператор spread
-  }
-
-  function deleteBook(id) 
-    {
-        setBooks(
-        // оставляем все книги, кроме той, что нужно удалить
-        books.filter(book => book.id !==id)
-        );
-    }
-
-  function updateBook(id) 
-  {
-    setBooks(
-      books.map(book => {
-
-        if(book.id === id)
-          {
-            return {
-              ...book,
-              title: `${book.title} (Updated)`
-            }
-          }
-
-        return book;
-
-      })
-   );
-  }
+  
 
   return (
     <Routes>
