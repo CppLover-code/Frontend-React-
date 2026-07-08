6f7f64445f89b959a0cff15c8198fef0cbc0b8e8
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-08 10:51:44 +0400

    !

--- Book_Library/src/contexts/CartContext.jsx
+++ Book_Library/src/contexts/CartContext.jsx
@@ -1,3 +1,12 @@
 import { createContext } from "react";
 
 const CartContext = createContext();
+
+// children здесь — это не данные корзины, 
+// а те компоненты, которым мы хотим дать доступ к корзине
+function CartProvider({children})
+{
+
+}
+
+export { CartContext, CartProvider };
