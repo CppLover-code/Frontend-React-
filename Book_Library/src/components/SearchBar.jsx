e12dbdf24d80c9054152d62b910ebd8f7f60e7a1
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-09 13:41:07 +0400

    !

--- Book_Library/src/components/SearchBar.jsx
+++ Book_Library/src/components/SearchBar.jsx
@@ -5,12 +5,15 @@ function SearchBar()
     const {searchQuery, setSearchQuery} = useBook();
 
     return (
+        <>
+            <p>Search:</p>
             <input
             type="text"
             value={searchQuery}
             onChange={(event) =>
                 setSearchQuery(event.target.value)
             }/>
+        </>
     );
 }
 
