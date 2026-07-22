9b8bcecf3bc4803469cca7e2dde94ddaf3b86215
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-22 14:27:11 +0400

    create table author and category

--- Book_Store/backend/books/models.py
+++ Book_Store/backend/books/models.py
@@ -10,7 +10,7 @@ class Author(models.Model):
 
 class Category(models.Model):
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
 
     def __str__(self):
         return self.name
