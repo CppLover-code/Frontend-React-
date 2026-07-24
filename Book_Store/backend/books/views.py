a358f344d8f6f7ee9038d279df7f0da9b454a984
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-24 13:07:34 +0400

    view

--- Book_Store/backend/books/views.py
+++ Book_Store/backend/books/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Book
+from .serializer import BookSerializer
 
-# Create your views here.
+class BookListAPIView(APIView):
+    
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+
