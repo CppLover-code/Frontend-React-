6d4c2c7c02a689a1e7137e4ba6bc9fac9fcc5980
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-28 15:24:26 +0400

    !

--- Book_Store/backend/books/urls.py
+++ Book_Store/backend/books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookListAPIView
+from .views import BookListAPIView, BookDetailAPIView
 
 urlpatterns = [
-    path("books/", BookListAPIView.as_view(), name="Book-list",)
+    path("books/", BookListAPIView.as_view(), name="Book-list"),
+    path("books/<int:pk>/", BookDetailAPIView.as_view(), name="Book-detail",)
 ]
--- Book_Store/backend/books/views.py
+++ Book_Store/backend/books/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Book
 from .serializer import BookSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class BookListAPIView(APIView):
     
@@ -23,3 +24,15 @@ class BookListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BookDetailAPIView(APIView):
+    
+    def get(self, request, pk):
+        
+        book = get_object_or_404(Book, pk=pk)
+        
+        serializer = BookSerializer(book)
+        
+        return Response(serializer.data)
+    
+    
