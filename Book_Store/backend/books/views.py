8f79031f8a7695faf4381297d9ee3566e5666d4c
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-07-30 12:34:44 +0400

    permissions

--- Book_Store/backend/books/views.py
+++ Book_Store/backend/books/views.py
@@ -1,8 +1,27 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import Book
 from .serializer import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class AllowEveryone(BasePermission):
+    def has_permission(self, request, view):
+        return True
+    
+class DenyEveryone(BasePermission):
+    def has_permission(self, request, view):
+        return False
+    
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return request.user.is_staff
+    
     
