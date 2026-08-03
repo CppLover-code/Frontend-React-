fac50ed3c718355f09d9fe590527417a030aa9a9
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-08-03 11:13:49 +0400

    сигналы

--- Book_Store/backend/cart/apps.py
+++ Book_Store/backend/cart/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
-
 class CartConfig(AppConfig):
-    name = 'cart'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cart"
+
+    # специальный метод, который вызывается при запуске приложения
+    # он нужен для того, чтобы зарегистрировать сигналы
+    def ready(self):
+        import cart.signals
