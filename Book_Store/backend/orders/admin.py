f4230c0697f41aeef3d778e5a6c6428b26877bd1
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-08-04 13:28:12 +0400

    admin register

--- Book_Store/backend/orders/admin.py
+++ Book_Store/backend/orders/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from .models import Order
+from .models import Order, OrderItem
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    
+    show_change_link = True
+    
+    readonly_fields = (
+        "book",
+        "book_title",
+        "book_cover",
+        "price",
+        "quantity",
+        "subtotal",
+    )
+    
+    # Disable delete option for OrderItemInline for admin interface.
+    can_delete = False
+    
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -13,10 +31,46 @@ class OrderAdmin(admin.ModelAdmin):
     ]
     list_filter = [
         "status",
+        "created_at",
     ]
     search_fields = [
+        "=id",
         "user__email",
     ]
     ordering = [
         "-created_at",
     ]
+    inlines = [
+        OrderItemInline,
+    ]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "order",
+        "book",
+        "book_title",
+        "quantity",
+        "price",
+        "subtotal",
+    )
+
+    search_fields = (
+        "book_title",
+        "order__user__email",
+    )
+
+    ordering = (
+        "-order__created_at",
+    )
+    
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
--- Book_Store/backend/orders/models.py
+++ Book_Store/backend/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from books.models import Book
 
 class Order(models.Model):
     class OrderStatus(models.TextChoices):
@@ -33,5 +34,46 @@ class Order(models.Model):
     
     def __str__(self):
         return f"Order {self.id} - {self.user.username}"
+
+class OrderItem(models.Model):
     
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items",
+        )
+    
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="order_items",
+        )
+    
+    book_title = models.CharField(
+        max_length=255
+        )
+    
+    book_cover = models.ImageField(
+        upload_to="book_covers/", 
+        null=True, 
+        blank=True
+        )
+    
+    quantity = models.PositiveIntegerField()
+    
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        )
+    
+    subtotal_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        )
+    
+    def __str__(self):
+        return f"OrderItem {self.id} - {self.book_title}"
    
