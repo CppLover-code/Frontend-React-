4d6d2e2df418b592cb625185765adbf01a6cfe0b
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-08-05 13:18:53 +0400

    order serializers.py done

---
+++ Book_Store/backend/orders/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Order, OrderItem
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = OrderItem
+        fields = [
+            "id",
+            "book",
+            "book_title",
+            "book_cover",
+            "price",
+            "quantity",
+            "subtotal",
+        ]
+
+class OrderSerializer(serializers.ModelSerializer):
+    
+    items = OrderItemSerializer(
+        many=True,
+        read_only=True
+        )
+    
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "user",
+            "status",
+            "total_price",
+            "created_at",
+            "updated_at",
+            "items",
+        ]
+        
+class CreateOrderSerializer(serializers.ModelSerializer):
+    pass
