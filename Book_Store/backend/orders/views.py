2a4e49f26456864a2b330e4c9bc0529177d97c47
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-08-06 11:10:50 +0400

    order view done

--- Book_Store/backend/orders/views.py
+++ Book_Store/backend/orders/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Order
+from .serializers import (
+    OrderSerializer,
+    CreateOrderSerializer,
+)
+from .services import create_order
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action == "create":
+            return CreateOrderSerializer
+        
+        return OrderSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        order = create_order(request.user)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
