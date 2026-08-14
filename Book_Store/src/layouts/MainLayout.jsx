11fb5e9ae10ae1e18d12619e9f48ba5397a73c4f
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2026-08-14 13:34:01 +0400

    mainlayout style editing

--- Book_Store/src/layouts/MainLayout.jsx
+++ Book_Store/src/layouts/MainLayout.jsx
@@ -5,16 +5,16 @@ import { Outlet } from "react-router-dom";
 function MainLayout()
 {
     return (
-        <>
+        <div className="flex min-h-screen flex-col bg-gray-50 text-gray-800">
             <Header />
 
-            <main>
+            <main className="mx-auto w-full max-w-6xl flex-1 px-4 py-8">
                 <Outlet />
             </main>
 
             <Footer />
 
-        </>
+        </div>
     );
 }
 
